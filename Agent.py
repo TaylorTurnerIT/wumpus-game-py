ce9646cd5573c68fa241511882f3e809771c7ab8
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-10-16 00:37:52 +0000

    Release Candidate

--- Agent.py
+++ Agent.py
@@ -1,6 +1,5 @@
 # Agent.py
 import random
-import typing
 from Percept import Percept
 from Action import GOFORWARD, TURNLEFT, TURNRIGHT, GRAB, SHOOT, CLIMB
 from Orientation import RIGHT, LEFT, UP, DOWN
@@ -32,22 +31,32 @@ class Agent:
         self.has_arrow: bool = True
         self.has_gold: bool = False
         self.orientation: int = RIGHT
-        pass
 
     def Turn(self, direction: int):
         if direction == TURNLEFT:
             self.orientation += 1
-            self.action = TURNLEFT
-        elif direction == TURNRIGHT:
+            return TURNLEFT
+        if direction == TURNRIGHT:
             self.orientation -= 1
-            self.action = TURNRIGHT
+            return TURNRIGHT
 
-    # def Move(self, direction: int):
-    #     """
-    #     Moves based on current orientation
-    #     """
-    #     if direction == GOFORWARD:
-    #     elif direction == TURN
+    def Move(self) -> int:
+        """
+        Moves based on current orientation
+        """
+        if self.orientation == LEFT:
+            # Increase x by 1
+            self.location[0] += 1
+        elif self.orientation == RIGHT:
+            # Decrease x by 1
+            self.location[0] -= 1
+        elif self.orientation == UP:
+            # Increase y by 1
+            self.location[1] += 1
+        elif self.orientation == DOWN:
+            # Decrease y by 1
+            self.location[1] -= 1
+        return GOFORWARD
 
     def Process(self, percept: Percept) -> int:
         """
@@ -65,12 +74,12 @@ class Agent:
                 action = CLIMB
             # If the agent has an arrow, and the agent is in the top row (Y=4),
             # and the agent’s orientation=RIGHT, then SHOOT.
-            elif self.has_arrow and self.orientation is RIGHT:
+            elif self.has_arrow and self.orientation is RIGHT and self.location[1] == 4:
                 action = SHOOT
                 self.has_arrow = False
             # If the agent has an arrow, and the agent is in the rightmost column (X=4),
             # and the agent’s orientation=UP, then SHOOT.
-            elif self.has_arrow and self.orientation is UP:
+            elif self.has_arrow and self.orientation is UP and self.location[0] == 4:
                 action = SHOOT
                 self.has_arrow = False
             # If none of the above conditions are met, then the agent should randomly choose one
@@ -79,12 +88,12 @@ class Agent:
                 choice = random.randint(0, 2)
                 match choice:
                     case 0:
-                        action = GOFORWARD
+                        action = self.Move()
                     case 1:
                         action = self.Turn(TURNLEFT)
                     case 2:
                         action = self.Turn(TURNRIGHT)
                     case _:
                         print(
-                            "Error in none case. Likely the random action generator is broken.")
+                            "Error in none case.")
             return action
